2ef67812c36d5fd2f9df344f4aad4740d4fb210a
SaraBaradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-01 20:49:54 -0400

    Add files via upload

--- Bank.py
+++ Bank.py
@@ -1,12 +1,12 @@
 #################################################################   
-#								#
-# 								#
-# 	Creator Name:   Sara Baradaran, Mahdi Heidari		#
-# 	Create Date:    Jun 2020 				#
-# 	Module Name:    Bank.py 				#
-# 	Project Name:   Reliable-Data-Transfer-Protocol 	#
-#								#
-#								#
+#								
+# 								
+# 	Creator Name:   Sara Baradaran, Mahdi Heidari		
+# 	Create Date:    Jun 2020 				
+# 	Module Name:    Bank.py 				
+# 	Project Name:   Reliable-Data-Transfer-Protocol 	
+#								
+#								
 #################################################################
 
 from raw_socket import *
