5e1c84e51d543ffec1a0a30e5374c78a03faace8
SaraBaradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-01 20:59:04 -0400

    Delete ip.sh

--- ip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo ifconfig $1:1 192.168.1.10 netmask 255.255.255.0 up
-sudo ifconfig $1:2 192.168.1.11 netmask 255.255.255.0 up
-sudo ifconfig $1:3 192.168.1.12 netmask 255.255.255.0 up
-sudo ifconfig $1:4 192.168.1.13 netmask 255.255.255.0 up
-sudo ifconfig $1:5 192.168.1.14 netmask 255.255.255.0 up
